47addc9dac2eed076320df505b736d491b28f32d
zhu <zhu@gmail.com>
2018-12-05 18:08:42 +0800

    2018-12-5配置gradle文件

--- app/src/main/java/com/massky/md5designer/app/App.java
+++ app/src/main/java/com/massky/md5designer/app/App.java
@@ -2,12 +2,17 @@ package com.massky.md5designer.app;
 
 import android.app.Application;
 
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.ViewTarget;
+import com.massky.md5designer.R;
+
 public class App extends Application {
 
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        ViewTarget.setTagId(R.id.glide_tag);
+      
     }
 }
