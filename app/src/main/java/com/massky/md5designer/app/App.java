2f4fd46eced09d2d68fc976edf6746eff0cf40e2
zhu <zhu@gmail.com>
2018-12-06 18:04:06 +0800

    2018-12-6配置weex,在Application中,,,,

--- app/src/main/java/com/massky/md5designer/app/App.java
+++ app/src/main/java/com/massky/md5designer/app/App.java
@@ -1,12 +1,13 @@
 package com.massky.md5designer.app;
 
 import android.app.Application;
-
-import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.bumptech.glide.request.target.ViewTarget;
 import com.massky.data.util.LoggerUtil;
 import com.massky.md5designer.BuildConfig;
 import com.massky.md5designer.R;
+import com.taobao.weex.InitConfig;
+import com.taobao.weex.WXSDKEngine;
+import com.taobao.weex.common.WXException;
 
 public class App extends Application {
 
@@ -16,8 +17,22 @@ public class App extends Application {
         super.onCreate();
         ViewTarget.setTagId(R.id.glide_tag);
         LoggerUtil.init(BuildConfig.DEBUG);
+        initWeex();
 
 
+    }
 
+    private void initWeex() {
+        InitConfig config = new InitConfig.Builder()
+                .setImgAdapter(null)
+                .setHttpAdapter(null)
+                .build();
+
+        WXSDKEngine.initialize(this, config);
+        try {
+            WXSDKEngine.registerComponent("tabPager", WXTabPagerComponent.class);
+        } catch (WXException e) {
+            LoggerUtil.d(e.getMessage());
+        }
     }
 }
