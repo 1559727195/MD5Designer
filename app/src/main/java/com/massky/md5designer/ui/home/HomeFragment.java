e3fa5982a7a64597b0013e03e6439ab35060f5e6
zhu <zhu@gmail.com>
2019-02-20 16:41:50 +0800

    2019-2-20 homeFragment ->解决SwipeRefreshLayout下拉刷新和AppBarLayout下拉冲突（已解决）；

--- app/src/main/java/com/massky/md5designer/ui/home/HomeFragment.java
+++ app/src/main/java/com/massky/md5designer/ui/home/HomeFragment.java
@@ -1,8 +1,14 @@
 package com.massky.md5designer.ui.home;
 
+import android.animation.ArgbEvaluator;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.TextView;
 
 import com.crazysunj.cardslideview.CardViewPager;
+import com.google.android.material.appbar.AppBarLayout;
 import com.massky.domain.entity.zhihu.ZhihuNewsEntity;
 import com.massky.md5designer.R;
 import com.massky.md5designer.base.BaseFragment;
@@ -14,6 +20,8 @@ import com.massky.md5designer.view.banner.BannerCardHandler;
 import com.massky.md5designer.view.banner.WrapBannerView;
 import java.util.List;
 import javax.inject.Inject;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -28,11 +36,19 @@ public class HomeFragment extends BaseFragment<NewHomePresenter> implements NewH
     WrapBannerView mWrapBanner;
     @BindView(R.id.home_list)
     RecyclerView mHomeList;
+    @BindView(R.id.home_appbar)
+    AppBarLayout mAppbar;
+    @BindView(R.id.home_title)
+    TextView mTitle;
+    @BindView(R.id.home_toolbar)
+    Toolbar mToolbar;
 
 
 
     @Inject
     HomeAdapter mAdapter;
+    private ArgbEvaluator mArgbEvaluator = new ArgbEvaluator();
+    private Drawable mNavigationIcon;
 
     @Override
     protected int getContentResId() {
@@ -42,21 +58,41 @@ public class HomeFragment extends BaseFragment<NewHomePresenter> implements NewH
     @Override
     protected void initListener() {
         mRefresh.setOnRefreshListener(this::onRefresh);
+        mAppbar.addOnOffsetChangedListener(this::handleAppbarOffsetChangedListener);
     }
 
     private void onRefresh() {
+        initData();
+    }
 
+    private void handleAppbarOffsetChangedListener(AppBarLayout appBarLayout, int verticalOffset) {
+        final int totalScrollRange = appBarLayout.getTotalScrollRange();
+        final float percent = Math.abs(verticalOffset * 1.0f / totalScrollRange);
+        mRefresh.setEnabled(verticalOffset == 0);
+        final int color = getColor(R.color.colorPrimary);
+//        mTitle.setTextColor((int) mArgbEvaluator.evaluate(percent, color, Color.WHITE));
+        mTitle.setTextColor((Integer) mArgbEvaluator.evaluate(percent,color,Color.WHITE));
+        if (mNavigationIcon != null) {
+            mNavigationIcon.setColorFilter((int) mArgbEvaluator.evaluate(percent, color, Color.WHITE), PorterDuff.Mode.SRC_IN);
+        }
     }
 
     @Override
     protected void initView() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = mActivity.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+        mToolbar.setNavigationIcon(R.mipmap.ic_scan);
+        mNavigationIcon = mToolbar.getNavigationIcon();
         mHomeList.setLayoutManager(new LinearLayoutManager(mActivity));
         mHomeList.setAdapter(mAdapter);
     }
 
     @Override
     public void showZhihu(ZhihuNewsEntity zhihuNewsEntity) {
-
+        stopRefresh();
         List<ZhihuNewsEntity.StoriesEntity> stories = zhihuNewsEntity.getStories();
         if (stories != null && !stories.isEmpty()) {
             mAdapter.notifyZhihuNewsList(stories);
@@ -69,6 +105,13 @@ public class HomeFragment extends BaseFragment<NewHomePresenter> implements NewH
         } else {
             mHomeBanner.setVisibility(View.VISIBLE);
             mHomeBanner.bind(mActivity.getSupportFragmentManager(), new BannerCardHandler(), topStories);
+        }
+    }
+
+
+    private void stopRefresh() {
+        if (mRefresh.isRefreshing()) {
+            mRefresh.setRefreshing(false);
         }
     }
 
